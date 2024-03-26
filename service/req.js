f42ccb2f2f48839d93d0ce54864e871961fed82d
WelintonJunior <welintonjr011@gmail.com>
2024-03-26 10:23:18 -0300

    Real Added the back end to the form Academia

---
+++ service/req.js
@@ -0,0 +1,45 @@
+//Form Create Academia
+
+document.getElementById('formAcademia').addEventListener('submit', handleSubmitFormAcademia)
+
+async function handleSubmitFormAcademia(e) {
+    e.preventDefault();
+    const fd = new FormData(e.target)
+    const data = Object.fromEntries(fd.entries())
+
+    const response = await fetch("http://localhost:3000/Academia", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            acao: "CreateAcademia",
+            data,
+        })
+    })
+
+    const result = await response.json()
+    console.log(result)
+}
+
+
+//Form Create Cliente
+
+// document.getElementById('formCliente').addEventListener('submit', handleSubmitFormCliente)
+
+// async function handleSubmitFormCliente(e) {
+//     e.preventDefault();
+//     const fd = new FormData(e.target)
+//     const data = Object.fromEntries(fd.entries())
+
+//     const response = await fetch("/Cliente", {
+//         method: "POST",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify({
+//             acao: "CreateCliente",
+//             data,
+//             idAcademia
+//         })
+//     })
+
+//     const result = await response.json()
+//     console.log(result)
+// }
