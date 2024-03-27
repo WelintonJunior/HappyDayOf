6a810b9b651bcef78949a7c59e17b757bb5e9c1a
WelintonJunior <welintonjr011@gmail.com>
2024-03-27 09:58:14 -0300

    More base to the back end, and example of login form

--- service/reqEquipe.js
+++ service/reqEquipe.js
@@ -19,27 +19,3 @@ async function handleSubmitFormAcademia(e) {
     const result = await response.json()
     console.log(result)
 }
-
-
-//Form Create Cliente
-
-// document.getElementById('formCliente').addEventListener('submit', handleSubmitFormCliente)
-
-// async function handleSubmitFormCliente(e) {
-//     e.preventDefault();
-//     const fd = new FormData(e.target)
-//     const data = Object.fromEntries(fd.entries())
-
-//     const response = await fetch("/Cliente", {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify({
-//             acao: "CreateCliente",
-//             data,
-//             idAcademia
-//         })
-//     })
-
-//     const result = await response.json()
-//     console.log(result)
-// }
