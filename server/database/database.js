5a8964db0e8dc1a2c74f07341c7248bf5b4153c4
WelintonJunior <welintonjr011@gmail.com>
2024-04-25 11:12:58 -0300

    Senha do banco no .env

--- server/database/database.js
+++ server/database/database.js
@@ -1,17 +1,19 @@
 const mysql = require("mysql2");
+require('dotenv').config({ path: "../.env" });
+const SENHA_BANCO = process.env.SENHA_BANCO;
 
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "2206",
-    database: "happyday2",
+  host: "localhost",
+  user: "root",
+  password: SENHA_BANCO,
+  database: "happyday2",
 });
 
 db.getConnection((err) => {
-    if (err) {
-      throw err;
-    }
-    console.log("Conexao estabelecida");
-  });
+  if (err) {
+    throw err;
+  }
+  console.log("Conexao estabelecida");
+});
 
 module.exports = db;
